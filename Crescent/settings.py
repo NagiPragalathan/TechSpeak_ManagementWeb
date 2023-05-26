b9e58ec082cbfe55f93a2708b19c1283c3cf3843
nagipragalathan <kk2069037@gmail.com>
2023-05-26 15:49:26 +0530

    settings.py are updated

--- Crescent/settings.py
+++ Crescent/settings.py
@@ -140,15 +140,12 @@ USE_TZ = True
 
 import os
 
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
