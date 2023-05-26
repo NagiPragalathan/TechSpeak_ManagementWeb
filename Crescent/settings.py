a5adaa97a4f8a9613c508397ee2662b04a6b9b7a
nagipragalathan <kk2069037@gmail.com>
2023-05-26 16:27:57 +0530

    sample commit

--- Crescent/settings.py
+++ Crescent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2%vvk$9n_6#p%uz-%(74(tcw=-um#c(8-!@7(um36xr+lk(y^q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DEPLOYMENT=True
+DEPLOYMENT=False
 
 if DEPLOYMENT:
     ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh','*']
